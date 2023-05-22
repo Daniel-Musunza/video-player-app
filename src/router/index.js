26470564af1d452328fc9e5802c7d92a4b6f21b2
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-05-22 14:55:34 +0300

    removing addData component

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
-import AddData from '../views/AddData.vue'
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/addData',
-    name: 'addData',
-    component: AddData
   },
 ]
 
